fa6a6b392f420f0f784c29e50a4faa537333b29b
WilsonRolando <r.tingo@hotmail.com>
2021-12-17 19:50:09 -0500

    agreaga funcion MISP

--- 2.permission-script.sh
+++ 2.permission-script.sh
@@ -2,8 +2,7 @@
 #!/bin/sh
 sudo chown -R 1000:1000 $PWD/ssl
 
-docker stop elasticsearch elasticsearch2 elasticsearch3 kibana elastalert cortex thehive
-
+docker stop elasticsearch elasticsearch2 elasticsearch3 kibana elastalert cortex thehive db misp misp-modules 
 #sudo chown -R 1000:1000 $PWD/volumes
 #sudo rm -rfv $PWD/cortex/Cortex-Analyzers
 
@@ -17,5 +16,10 @@ sudo chown -R 1000:1000 $PWD/theHive
 
 sudo chown -R 1000:1000 $PWD/cortex
 
+sudo chown -R 1000:1000 $PWD/misp
 
-docker start elasticsearch elasticsearch2 elasticsearch3 kibana elastalert cortex thehive
+# sudo chown -R 1000:1000 $PWD/vol
+
+# sudo chown -R 1000:1000 $PWD/vol/mysql
+
+docker start elasticsearch elasticsearch2 elasticsearch3 kibana elastalert cortex thehive db misp misp-modules 
