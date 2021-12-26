6d893efcab425a229e996cdfb2458009036daead
WilsonRolando <r.tingo@hotmail.com>
2021-12-26 11:48:12 -0500

    funcionando con nombres del servicio excepto en thehive su application.conf seccion de MISP = localhost provoca iniciar sesion nuevamente, igual con la IP. El archivo .env con nlas ip/24  y un solo servicio REDIS para logstash y MISP, funcionando

--- 2.permission-script.sh
+++ 2.permission-script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 #!/bin/sh
-sudo chown -R 1000:1000 $PWD/ssl
+
+#elasticsearch
+sudo sysctl -w  vm.max_map_count=262144
+
+#redis
+sudo sysctl vm.overcommit_memory=1
+
+
 echo stop................
-docker stop elasticsearch1 elasticsearch2 elasticsearch3 kibana elastalert cortex thehive db misp misp-modules 
+docker stop elasticsearch1 elasticsearch2 elasticsearch3 kibana elastalert cortex thehive redis db misp misp-modules 
 #sudo chown -R 1000:1000 $PWD/volumes
 #sudo rm -rfv $PWD/cortex/Cortex-Analyzers
-
+sudo chown -R 1000:1000 $PWD/ssl
 
 sudo chown -R 1000:1000 $PWD/kibana
 
@@ -19,11 +26,11 @@ sudo chown -R 1000:1000 $PWD/theHive
 
 sudo chown -R 1000:1000 $PWD/cortex
 
-sudo chown -R 1000:1000 $PWD/misp
+#sudo chown -R 1000:1000 $PWD/misp
 
+sudo chown -R 1000:1000 $PWD/volmysql
 
-# sudo chown -R 1000:1000 $PWD/vol
+#sudo chown -R 1000:1000 $PWD/redis_logst
 
-# sudo chown -R 1000:1000 $PWD/vol/mysql
 echo start.................
-docker start elasticsearch1 elasticsearch2 elasticsearch3 kibana elastalert cortex thehive db misp misp-modules 
+docker start elasticsearch1 elasticsearch2 elasticsearch3 kibana elastalert cortex thehive redis db misp misp-modules 
