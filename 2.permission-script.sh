5dd507e5645342846506227df92575822c34970e
WilsonRolando <r.tingo@hotmail.com>
2022-01-04 11:33:38 -0500

    update permissions

--- 2.permission-script.sh
+++ 2.permission-script.sh
@@ -21,7 +21,7 @@ docker exec redis bash -c "chmod 777 /etc/cron.d"
 docker stop elasticsearch1 elasticsearch2 elasticsearch3 kibana elastalert cortex thehive redis db misp misp-modules 
 
 # sudo chown -R 1000:1000 $PWD/ssl
-sudo chmod -R 777 $PWD/ssl
+#sudo chmod -R 777 $PWD/ssl
 # sudo chown -R 1000:1000 $PWD/kibana
 sudo chmod -R 777 $PWD/kibana
 # sudo chown -R 1000:1000 $PWD/logstash
@@ -35,9 +35,9 @@ sudo chmod -R 777 $PWD/theHive
 # sudo chown -R 1000:1000 $PWD/cortex
 sudo chmod -R 777 $PWD/cortex
 # #sudo chown -R 1000:1000 $PWD/misp
-sudo chmod -R 777 $PWD/misp
+#sudo chmod -R 777 $PWD/misp
 # sudo chown -R 1000:1000 $PWD/volmysql
-sudo chmod -R 777 $PWD/volmysql
+#sudo chmod -R 777 $PWD/volmysql
 
 # #sudo chown -R 1000:1000 $PWD/redis_logst
 
